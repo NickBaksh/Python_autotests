25fd5e43d06c90dea2b6c9a1be56f7a1065b3928
Ник Бак <kote981030@gmail.com>
2020-08-26 13:37:12 +0300

    Тестирование страницы яндекс поиска

--- test_yandex_search.py
+++ test_yandex_search.py
@@ -1,21 +1,34 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+import allure
+from allure import severity, severity_level
 
 
+@allure.title('Количество результатов поиска больше 10')
+@severity(severity_level.CRITICAL)
 def test_yandex_search():
     driver = WebDriver(executable_path='C://Chrome driver 84//chromedriver.exe')
-    driver.get('http://www.ya.ru')
-    search_input = driver.find_element_by_xpath('//input[@id="text"]')
-    search_button = driver.find_element_by_xpath('//div[@class="search2__button"]//button[@type="submit"]')
-    search_input.send_keys('market.yandex.ru')
-    search_button.click()
+    with allure.step('Открываем страницу поиска'):
+        driver.get('http://www.ya.ru')
 
-    wait = WebDriverWait(driver, 10)
+    with allure.step('Открываем страницу market.yandex'):
+        search_input = driver.find_element_by_xpath('//input[@id="text"]')
+        search_button = driver.find_element_by_xpath('//div[@class="search2__button"]//button[@type="submit"]')
+        search_input.send_keys('market.yandex.ru')
+        search_button.click()
 
-    wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="organic__url-text"]')))
-    search_results = driver.find_elements_by_xpath('//li[@class="serp-item"]')
-    link = search_results[0].find_element_by_xpath('.//h2/a')
-    link.click()
+    def check_results_count(driver):
+        inner_search_results = driver.find_elements_by_xpath('//li[@class="serp-item"]')
+        return len(inner_search_results) >= 10
+
+    with allure.step('Проверяем количество результатов поиска'):
+        WebDriverWait(driver, 5, 0.5).until(check_results_count, 'The count less than 10')
+
+    with allure.step('По первому результату переходим на страницу market.yandex'):
+        search_results = driver.find_elements_by_xpath('//li[@class="serp-item"]')
+        link = search_results[0].find_element_by_xpath('.//h2/a')
+        link.click()
+
     driver.switch_to.window(driver.window_handles[1])
+    with allure.step('Проверяем Title на корректность'):
+        assert driver.title == 'Яндекс.Маркет — выбор и покупка товаров из проверенных интернет-магазинов'
